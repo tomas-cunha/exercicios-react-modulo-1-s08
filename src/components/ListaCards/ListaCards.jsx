33c0b6a7124f698c4841e62fe747e34b524a5050
tomas-cunha <tcunha930@gmail.com>
2022-08-25 15:39:25 -0300

    ex03 - estilo condicional

--- src/components/ListaCards/ListaCards.jsx
+++ src/components/ListaCards/ListaCards.jsx
@@ -8,7 +8,7 @@ export const ListaCards = ({ produtos }) => (
   <ul className={styles.lista}>
     {produtos.map((produto) => (
       <li className={styles.listaItem} key={produto.id}>
-        <Card produto={produto} />
+        <Card produto={produto} selecionado={false} />
       </li>
     ))}
   </ul>
