3b6702a6f9bffdd5753a3e38444018d4aeea9c7e
tomas-cunha <tcunha930@gmail.com>
2022-08-25 16:27:40 -0300

    ex05 - modificando listacards

--- src/components/ListaCards/ListaCards.jsx
+++ src/components/ListaCards/ListaCards.jsx
@@ -4,15 +4,21 @@ import { Card } from '@components';
 
 import styles from './ListaCards.module.css';
 
-export const ListaCards = ({ produtos }) => (
-  <ul className={styles.lista}>
-    {produtos.map((produto) => (
-      <li className={styles.listaItem} key={produto.id}>
-        <Card produto={produto} selecionado={false} onSelecionar={() => console.log('clicou')} />
-      </li>
-    ))}
-  </ul>
-);
+import { useState } from 'react';
+
+export const ListaCards = ({ produtos }) => {
+  const [idSelecionados, setIdSelecionados] = useState([])
+
+  return (
+    <ul className={styles.lista}>
+      {produtos.map((produto) => (
+        <li className={styles.listaItem} key={produto.id}>
+          <Card produto={produto} selecionado={false} onSelecionar={() => console.log('clicou')} />
+        </li>
+      ))}
+    </ul>
+  )
+};
 
 ListaCards.propTypes = {
   produtos: PropTypes.array.isRequired,
