8e59e6723ab3a7657a4ffaa94b0b1239d6ba55a4
tomas-cunha <tcunha930@gmail.com>
2022-08-30 14:57:12 -0300

    exercicios 01 e 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Footer, Header, Secao, FiltroSecao } from '@components';
-import { produtos } from '@services';
-import { useState } from 'react';
-import styles from './App.module.css';
+import { Footer, Header, Secao, FiltroSecao } from "@components";
+import { produtos } from "@services";
+import { ProdutosSelecionadosProvider } from "./context/ProdutosSelecionados/index";
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
   const [filtro, setFiltro] = useState(null);
@@ -13,7 +14,9 @@ function App() {
   };
 
   const obterSubSecoes = (secao) => {
-    const produtosComSubSecoes = obterProdutosSecao(secao).filter((p) => p.subSecao);
+    const produtosComSubSecoes = obterProdutosSecao(secao).filter(
+      (p) => p.subSecao
+    );
 
     return Array.from(new Set(produtosComSubSecoes.map((p) => p.subSecao)));
   };
@@ -35,24 +38,26 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <Header />
-      <main className={styles.main}>
-        <FiltroSecao
-          secoes={secoes}
-          secaoSelecionada={filtro}
-          onSelecionar={handleSelecionarSecao}
-        />
-
-        {obterSecoesFiltradas().map((secao) => (
-          <Secao
-            key={secao}
-            nome={secao}
-            produtos={obterProdutosSecao(secao)}
-            subSecoes={obterSubSecoes(secao)}
+      <ProdutosSelecionadosProvider>
+        <Header />
+        <main className={styles.main}>
+          <FiltroSecao
+            secoes={secoes}
+            secaoSelecionada={filtro}
+            onSelecionar={handleSelecionarSecao}
           />
-        ))}
-      </main>
-      <Footer />
+
+          {obterSecoesFiltradas().map((secao) => (
+            <Secao
+              key={secao}
+              nome={secao}
+              produtos={obterProdutosSecao(secao)}
+              subSecoes={obterSubSecoes(secao)}
+            />
+          ))}
+        </main>
+        <Footer />
+      </ProdutosSelecionadosProvider>
     </div>
   );
 }
---
+++ src/context/ProdutosSelecionados/ProdutosSelecionadosProvider.jsx
@@ -0,0 +1,34 @@
+import { ProdutosSelecionadosContext } from "./ProdutosSelecionadosContext";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export const ProdutosSelecionadosProvider = ({ children }) => {
+  const [produtosSelecionados, setProdutosSelecionados] = useState([]);
+
+  const isSelecionado = (id) => {
+    return produtosSelecionados.some((item) => item === id);
+  };
+
+  const handleSelecionar = (produto) => {
+    if (isSelecionado(produto.id)) {
+      setProdutosSelecionados(
+        produtosSelecionados.filter((item) => item.id !== produto.id)
+      );
+      return;
+    }
+
+    setProdutosSelecionados([...produtosSelecionados, produto]);
+  };
+
+  return (
+    <ProdutosSelecionadosContext.Provider
+      value={{ produtosSelecionados, isSelecionado, handleSelecionar }}
+    >
+      {children}
+    </ProdutosSelecionadosContext.Provider>
+  );
+};
+
+ProdutosSelecionadosProvider.propTypes = {
+  children: PropTypes.node,
+};
