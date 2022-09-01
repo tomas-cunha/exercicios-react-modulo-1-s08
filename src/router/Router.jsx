d3910f851d0e015503c335df09081b9c65791b33
tomas-cunha <tcunha930@gmail.com>
2022-09-01 11:39:32 -0300

    exercicio07

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,11 @@
-import { Produtos } from "@pages";
+import { Produtos, ProdutosSelecionados } from "@pages";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/produtos" element={<Produtos />} />
+      <Route path="/produtos-selecionados" element={<ProdutosSelecionados />} />
       <Route path="*" element={<Navigate to={"/produtos"} replace={true} />} />
     </Routes>
   );
