e1525f1c9e7efc8d7bde420fb35bf42bdf533eac
tomas-cunha <tcunha930@gmail.com>
2022-09-12 22:26:40 -0300

    ex10

--- src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -1,9 +1,9 @@
 import { ListaCards } from "@components";
 import { useProdutosSelecionados } from "@context";
+import styles from "./ProdutosSelecionados.module.css";
 
 export const ProdutosSelecionados = () => {
-  const { produtosSelecionados } = useProdutosSelecionados();
-  console.log(produtosSelecionados);
+  const { produtosSelecionados, limparProdutos } = useProdutosSelecionados();
 
   const total = produtosSelecionados.reduce((soma, item) => {
     return soma + item.valor;
@@ -11,7 +11,13 @@ export const ProdutosSelecionados = () => {
 
   return (
     <>
-      <h1>Produtos Selecionados</h1>
+      <div className={styles.container}>
+        <h1>Produtos Selecionados</h1>
+        <button className={styles.botaoDelete} onClick={() => limparProdutos()}>
+          Remover itens
+        </button>
+      </div>
+
       <h2>R$ {total.toFixed(2).replace(".", `,`)}</h2>
       <ListaCards produtos={produtosSelecionados} />
     </>
